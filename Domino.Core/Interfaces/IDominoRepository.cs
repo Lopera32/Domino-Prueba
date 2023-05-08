5b2e0c4560fc1825cb935dd022f063911fd61834
Sebastian <jsloperagdv@gmail.com>
2023-05-07 21:08:19 -0500

    Domino project

---
+++ Domino.Core/Interfaces/IDominoRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domino.Core.DTOs;
+using Domino.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domino.Core.Interfaces
+{
+    public interface IDominoRepository
+    {
+        Task<IEnumerable<DominoFullGame>> GetDominoPieces();
+        List<DominoDto> CalculateChain(List<DominoDto> dominoPiece);
+    }
+}
---
+++ Domino.Infrastructure/Repositories/DominoPieceRepository.cs
@@ -0,0 +1,93 @@
+﻿using Domino.Core.DTOs;
+using Domino.Core.Entities;
+using Domino.Core.Interfaces;
+using Domino.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Domino.Infrastructure.Repositories
+{
+    public class DominoPieceRepository : IDominoRepository
+    {
+        private readonly DominoContext _context;
+        public DominoPieceRepository(DominoContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<DominoFullGame>> GetDominoPieces()
+        {
+            var dominoPieces = await _context.dominoFullGames.ToListAsync();
+
+            return dominoPieces;
+        }
+
+   
+
+
+        public List<DominoDto> CalculateChain(List<DominoDto> dominoPiece)
+        {
+            var availableDominoPiece = new List<DominoDto>(dominoPiece);
+
+            var chain = new List<DominoDto> { availableDominoPiece[0] };
+            availableDominoPiece.RemoveAt(0);
+
+            while (availableDominoPiece.Count > 0)
+            {
+                var matchFound = false;
+
+                for (var i = 0; i < availableDominoPiece.Count; i++)
+                {
+                    if (chain[0].FirstValue == availableDominoPiece[i].SecondValue)
+                    {
+                        chain.Insert(0, availableDominoPiece[i]);
+                        availableDominoPiece.RemoveAt(i);
+                        matchFound = true;
+                        break;
+                    }
+                    else if (chain[0].FirstValue == availableDominoPiece[i].FirstValue)
+                    {
+                        chain.Insert(0, availableDominoPiece[i]);
+                        availableDominoPiece.RemoveAt(i);
+                        matchFound = true;
+                        break;
+                    }
+                }
+
+                if (matchFound)
+                {
+                    continue;
+                }
+
+                for (var i = 0; i < availableDominoPiece.Count; i++)
+                {
+                    if (chain[chain.Count - 1].SecondValue == availableDominoPiece[i].FirstValue)
+                    {
+                        chain.Add(availableDominoPiece[i]);
+                        availableDominoPiece.RemoveAt(i);
+                        matchFound = true;
+                        break;
+                    }
+                    else if (chain[chain.Count - 1].SecondValue == availableDominoPiece[i].SecondValue)
+                    {
+                        chain.Add(availableDominoPiece[i]);
+                        availableDominoPiece.RemoveAt(i);
+                        matchFound = true;
+                        break;
+                    }
+                }
+
+                if (!matchFound)
+                {
+                    return null;
+                }
+            }
+
+            return chain;
+        }
+
+
+
+    }
+}
